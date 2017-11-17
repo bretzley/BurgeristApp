8f37c3fb885737a8c81ee92dd84afe91dc13382a
bretzley <bretzley@gotribit.com>
2017-11-16 23:27:06 -0800

    +Working on login

--- Burgerist/app/src/main/java/com/example/bretz/burgerist/MainActivity.java
+++ Burgerist/app/src/main/java/com/example/bretz/burgerist/MainActivity.java
@@ -1,13 +1,91 @@
 package com.example.bretz.burgerist;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CompoundButton;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button btnLogin, btnRegister;
+    RadioButton rbTec, rbUser;
+    RadioGroup rgLogin;
+    TextView txtLogin;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btnLogin = (Button) findViewById(R.id.btnMainLogin);
+        btnRegister = (Button) findViewById(R.id.btnRegister);
+        rbUser = (RadioButton) findViewById(R.id.rbUser);
+        rbTec = (RadioButton) findViewById(R.id.rbTec);
+        rgLogin = (RadioGroup) findViewById(R.id.rgLogin);
+        txtLogin = (TextView) findViewById(R.id.txtWelcomeLogin);
+        rbUser.setOnCheckedChangeListener(radioListener);
+        rbTec.setOnCheckedChangeListener(radioListener);
+
+
+
+
+
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intentLogin= new Intent(getApplicationContext(), UserLoginActivity.class);
+                boolean checked = ((RadioButton)v).isChecked();
+
+                /*switch (v.getId()){
+                    case R.id.rbTec:
+                        if(checked){
+                            txtLogin.setText("¡Bienvenido Tecnico!");
+                        }
+                        break;
+                    case R.id.rbUser:
+                        if(checked) {
+                            txtLogin.setText("¡Bienvenido Cliente Distinguido!");
+                        }
+                        break;
+                    default:
+                        Toast.makeText(getApplicationContext(),"Error! Seleccione el tipo de usuario para iniciar sesion.",Toast.LENGTH_LONG);
+                        break;
+
+                }*/
+                startActivity(intentLogin);
+            }
+        });
+
+
+        btnRegister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), RegisterActivity.class);
+                startActivity(intent);
+            }
+        });
     }
+
+
+
+    final CompoundButton.OnCheckedChangeListener radioListener =
+            new CompoundButton.OnCheckedChangeListener()
+            {
+                @Override
+                public void onCheckedChanged(CompoundButton buttonView, boolean isChecked)
+                {
+                    if (!isChecked)
+                        return;
+                    if (buttonView.getId() == R.id.rbUser)
+                        Toast.makeText(getApplicationContext(), "Seleccionaste ingresar como usuario", Toast.LENGTH_SHORT).show();
+                    else if (buttonView.getId() == R.id.rbTec)
+                        Toast.makeText(getApplicationContext(), "Seleccionaste ingresar como tecnico", Toast.LENGTH_SHORT).show();
+                }
+            };
 }
--- Burgerist/app/src/main/java/com/example/bretz/burgerist/UserLoginActivity.java
+++ Burgerist/app/src/main/java/com/example/bretz/burgerist/UserLoginActivity.java
@@ -1,13 +1,45 @@
 package com.example.bretz.burgerist;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class UserLoginActivity extends AppCompatActivity {
+
+    private Button btnUserLog;
+    private EditText edtUser, edtPass;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_login);
+
+        btnUserLog = (Button)findViewById(R.id.btnUserLogin);
+        edtPass = (EditText)findViewById(R.id.edtLoginUserPass);
+        edtUser = (EditText)findViewById(R.id.edtLoginUser);
+
+        btnUserLog.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                if (edtUser.getText()== null) {
+                    Toast.makeText(getApplicationContext(), "Por favor ingresa tu usuario", Toast.LENGTH_SHORT).show();
+                } else if (edtPass.getText()== null) {
+                    Toast.makeText(getApplicationContext(), "Por favor ingresa tu contraseña", Toast.LENGTH_SHORT).show();
+                }
+
+                if (edtUser.getText().toString().equals("user") && edtPass.getText().toString().equals("pass")){
+                    Toast.makeText(getApplicationContext(),"Username and password is correct", Toast.LENGTH_SHORT).show();
+                    Intent intent = new Intent(getApplicationContext(), UserProfileActivity.class);
+                    startActivity(intent);
+
+                }
+                else Toast.makeText(getApplicationContext(), "Username and password is NOT correct", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
