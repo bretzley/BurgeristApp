50a25d1ed21e6b228a785d6d7b31b87232d278f3
bretzley <bretzley@gotribit.com>
2017-11-24 19:54:14 -0800

    profile activity

--- Burgerist/app/src/main/java/com/example/bretz/burgerist/UserProfileActivity.java
+++ Burgerist/app/src/main/java/com/example/bretz/burgerist/UserProfileActivity.java
@@ -2,12 +2,41 @@ package com.example.bretz.burgerist;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
+
+import com.example.bretz.burgerist.Objects.Customer;
+import com.example.bretz.burgerist.Utils.DBHelper;
 
 public class UserProfileActivity extends AppCompatActivity {
+
+    private TextView txtUserName, txtUserContract, txtUserPhone, txtUserEmail;
+    DBHelper db;
+    String contract, email;
+    Customer sessionCustomer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_profile);
+
+        txtUserName = (TextView)findViewById(R.id.txtUserName);
+        txtUserContract = (TextView)findViewById(R.id.txtUserContract);
+        txtUserPhone = (TextView)findViewById(R.id.txtUserPhone);
+        txtUserEmail = (TextView)findViewById(R.id.txtUserEmail);
+        db = new DBHelper(this);
+        contract = getIntent().getStringExtra("Contract");
+        email = getIntent().getStringExtra("Email");
+        sessionCustomer = new Customer();
+
+        sessionCustomer = db.getCustomer(email);
+
+        txtUserName.setText(sessionCustomer.getFirstName() + " " + sessionCustomer.getLastName());
+        txtUserContract.setText(sessionCustomer.getContractNumber());
+        txtUserPhone.setText(sessionCustomer.getPhone());
+        txtUserEmail.setText(sessionCustomer.getEmail());
+
+
     }
+
+
 }
