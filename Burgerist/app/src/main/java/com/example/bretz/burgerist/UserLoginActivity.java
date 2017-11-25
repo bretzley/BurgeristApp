faab2963a0ad222385295592a23b5a2143755b29
bretzley <bretzley@gotribit.com>
2017-11-24 18:17:18 -0800

    +Appt DB

--- Burgerist/app/src/main/java/com/example/bretz/burgerist/UserLoginActivity.java
+++ Burgerist/app/src/main/java/com/example/bretz/burgerist/UserLoginActivity.java
@@ -8,10 +8,14 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import com.example.bretz.burgerist.Objects.Customer;
+import com.example.bretz.burgerist.Utils.DBHelper;
+
 public class UserLoginActivity extends AppCompatActivity {
 
     private Button btnUserLog;
     private EditText edtUser, edtPass;
+    DBHelper db;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,10 +25,12 @@ public class UserLoginActivity extends AppCompatActivity {
         btnUserLog = (Button)findViewById(R.id.btnUserLogin);
         edtPass = (EditText)findViewById(R.id.edtLoginUserPass);
         edtUser = (EditText)findViewById(R.id.edtLoginUser);
+        db = new DBHelper(this);
 
         btnUserLog.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), UserProfileActivity.class);
 
                 if (edtUser.getText()== null) {
                     Toast.makeText(getApplicationContext(), "Por favor ingresa tu usuario", Toast.LENGTH_SHORT).show();
@@ -32,13 +38,22 @@ public class UserLoginActivity extends AppCompatActivity {
                     Toast.makeText(getApplicationContext(), "Por favor ingresa tu contraseña", Toast.LENGTH_SHORT).show();
                 }
 
-                if (edtUser.getText().toString().equals("user") && edtPass.getText().toString().equals("pass")){
-                    Toast.makeText(getApplicationContext(),"Usuario o contraseña son INCORRECTOS", Toast.LENGTH_SHORT).show();
-                    Intent intent = new Intent(getApplicationContext(), UserProfileActivity.class);
+                String user = edtUser.getText().toString();
+                String pass = edtPass.getText().toString();
+                Customer cus = new Customer();
+
+                try {cus = db.getCustomer(user);}catch(Exception e){
+                    Toast.makeText(getApplicationContext(), "El usuario no existe!", Toast.LENGTH_SHORT).show();
+                }
+
+                if (user.equals(cus.getEmail()) && pass.equals(cus.getPassword())){
+                    Toast.makeText(getApplicationContext(),"Bienvenido!", Toast.LENGTH_SHORT).show();
                     startActivity(intent);
 
                 }
                 else Toast.makeText(getApplicationContext(), "Usuario o contraseña son INCORRECTOS", Toast.LENGTH_SHORT).show();
+
+                //startActivity(intent);
             }
         });
     }
--- Burgerist/app/src/main/java/com/example/bretz/burgerist/Utils/DBUtils.java
+++ Burgerist/app/src/main/java/com/example/bretz/burgerist/Utils/DBUtils.java
@@ -40,6 +40,23 @@ public class DBUtils extends SQLiteOpenHelper{
     public static final String EMPLOYEE_PHONE = "Phone";
     public static final String EMPLOYEE_IMAGE = "EmployeeImage";
 
+    //-----------------APPOINTMENT TABLE---------------------------
+    public static final String APPOINTMENT_TABLE ="Appointment";
+    public static final String APPOINTMENT_BASEID ="ApptID";
+    public static final String APPOINTMENT_DATE = "date";
+    public static final String APPOINTMENT_TIMESLOTID = "timeSlotID";
+    public static final String APPOINTMENT_CUSTOMERID = "customerID";
+
+    //-----------------APPOINTMENT DETAILS TABLE---------------------------
+    public static final String APPTDET_TABLE ="AppointmentDetail";
+    public static final String APPTDET_BASEID ="apptDetalID";
+    public static final String APPTDET_APPTID = "apptID";
+    public static final String APPTDET_START = "startTime";
+    public static final String APPTDET_END = "endTime";
+    public static final String APPTDETE_TECHNOTES = "techNotes";
+    public static final String APPTDET_CUSTOMERNOTES = "customerNotes";
+    public static final String APPTDET_EMPLOYEEID = "employeeID";
+    public static final String APPTDET_RATING = "rating";
 
 
     public static final String CREATE_CUSTOMERS =
@@ -69,6 +86,24 @@ public class DBUtils extends SQLiteOpenHelper{
                     EMPLOYEE_PHONE + " INTEGER," +
                     EMPLOYEE_IMAGE + " TEXT)";
 
+    public static final String CREATE_APPOINTMENT =
+            "CREATE TABLE "+ APPOINTMENT_TABLE + "(" +
+                    APPOINTMENT_BASEID+" INTEGER PRIMARY KEY AUTOINCREMENT, " +
+                    APPOINTMENT_DATE + " DATETIME, " +
+                    APPOINTMENT_TIMESLOTID + " INTEGER, " +
+                    APPOINTMENT_CUSTOMERID + " INTEGER)";
+
+    public static final String CREATE_APPTDET =
+            "CREATE TABLE "+ APPTDET_TABLE + "(" +
+                    APPTDET_BASEID+" INTEGER PRIMARY KEY AUTOINCREMENT, " +
+                    APPTDET_APPTID + " INTEGER, " +
+                    APPTDET_START + " DATETIME, " +
+                    APPTDET_END+ " DATETIME," +
+                    APPTDETE_TECHNOTES + " TEXT," +
+                    APPTDET_CUSTOMERNOTES + " TEXT," +
+                    APPTDET_EMPLOYEEID + " INTEGER," +
+                    APPTDET_RATING + " INTEGER)";
+
     public DBUtils(Context context) {
         super(context, DATABASE_NAME, null, DATABASE_VERSION);
     }
@@ -77,12 +112,16 @@ public class DBUtils extends SQLiteOpenHelper{
     public void onCreate(SQLiteDatabase db) {
         db.execSQL(CREATE_CUSTOMERS);
         db.execSQL(CREATE_EMPLOYEES);
+        db.execSQL(CREATE_APPOINTMENT);
+        db.execSQL(CREATE_APPTDET);
     }
 
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
         db.execSQL("DROP TABLE IF EXISTS " + CUSTOMER_TABLE + "");
         db.execSQL("DROP TABLE IF EXISTS " + EMPLOYEE_TABLE + "");
+        db.execSQL("DROP TABLE IF EXISTS " + APPOINTMENT_TABLE + "");
+        db.execSQL("DROP TABLE IF EXISTS " + APPTDET_TABLE + "");
         onCreate(db);
     }
 }
