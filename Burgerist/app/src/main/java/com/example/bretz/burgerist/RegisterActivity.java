b7861629f0212f9f67b67baa2d33bedc35abadfa
Brandon Garcia Quezada <brandongq@gmail.com>
2017-12-01 11:04:55 -0800

    Details about the user registration, view done

--- Burgerist/app/src/main/java/com/example/bretz/burgerist/RegisterActivity.java
+++ Burgerist/app/src/main/java/com/example/bretz/burgerist/RegisterActivity.java
@@ -29,10 +29,12 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
 
+import static android.widget.Toast.*;
+
 public class RegisterActivity extends AppCompatActivity {
 
     EditText edtUserContract, edtUserEmail, edtUserPass;
-    Button btnRegisterUser, btnLookForContractNo ;
+    Button btnRegisterUser, btnLookForContractNo;
     DBHelper db;
     ArrayList<Customer> customers;
 
@@ -51,20 +53,18 @@ public class RegisterActivity extends AppCompatActivity {
 
         edtUserEmail.setEnabled(false);
         edtUserPass.setEnabled(false);
-        //edtUserEmail.setEnabled(true);
-        //edtUserPass.setEnabled(true);
         btnRegisterUser.setVisibility(View.INVISIBLE);
 
         final RequestQueue queue = Volley.newRequestQueue(this);
         final String customerAPI = "http://ec2-34-226-122-227.compute-1.amazonaws.com:2403/customer";
         final Context myContext = this;
 
-        btnLookForContractNo.setOnClickListener(new View.OnClickListener(){
+        btnLookForContractNo.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 if (edtUserContract.getText() == null) {
-                    Toast.makeText(getApplicationContext(), "Por favor, escribe un numero de contrato.", Toast.LENGTH_SHORT).show();
-                }else {
+                    makeText(getApplicationContext(), "Por favor, escribe un numero de contrato.", LENGTH_SHORT).show();
+                } else {
                     /*db.open();
                     db.addCustomer("a", 1, "a", "a", "a", "a", "a", 1, "a", true);
                     db.close();*/
@@ -84,46 +84,38 @@ public class RegisterActivity extends AppCompatActivity {
                                                 String id = rCustomer.getString("id");
                                                 int contractNo = Integer.parseInt(contractNumber);
                                                 String lastName = rCustomer.getString("LastName");
-                                                String email = rCustomer.getString("Email");
-                                                String password = rCustomer.getString("Password");
+                                                String email = rCustomer.has("Email") ? rCustomer.getString("Email") : "";
+                                                String password = rCustomer.has("Password") ? rCustomer.getString("Password") : "";
                                                 String address = rCustomer.getString("Address");
                                                 int phone = rCustomer.getInt("Phone");
-                                                String image = rCustomer.getString("Image");
+                                                String image = rCustomer.has("Image") ? rCustomer.getString("Image") : "";
                                                 db.open();
                                                 db.addCustomer(id, contractNo, name, lastName, email, password, address, phone, image, registered);
                                                 db.close();
-                                            } else
-                                                Toast.makeText(getApplicationContext(), name + " ya esta registrado.", Toast.LENGTH_SHORT).show();
+                                                btnRegisterUser.setVisibility(View.VISIBLE);
+                                                btnLookForContractNo.setVisibility(View.INVISIBLE);
+                                                edtUserContract.setEnabled(false);
+                                                edtUserEmail.setEnabled(true);
+                                                edtUserPass.setEnabled(true);
+                                            } else {
+                                                makeText(getApplicationContext(), name + ", ya esta registrado. Por favor, inice sesion.", LENGTH_SHORT).show();
+                                                Intent intent = new Intent(getApplicationContext(), UserLoginActivity.class);
+                                                startActivity(intent);
+                                            }
                                         } catch (JSONException e) {
-                                            Toast.makeText(myContext, "El numero de contrato no existe.", Toast.LENGTH_SHORT).show();
+                                            makeText(myContext, "El numero de contrato no existe.", LENGTH_SHORT).show();
                                         }
                                     } else
-                                        Toast.makeText(getApplicationContext(), "El numero de contrato no existe. No response", Toast.LENGTH_SHORT).show();
+                                        makeText(getApplicationContext(), "El numero de contrato no existe. No response", LENGTH_SHORT).show();
                                 }
                             }, new Response.ErrorListener() {
                                 @Override
                                 public void onErrorResponse(VolleyError error) {
-                                    Toast.makeText(getApplicationContext(), "Oops, algo salio mal. Intente en un momento.", Toast.LENGTH_SHORT).show();
+                                    makeText(getApplicationContext(), "Oops, algo salio mal. Intente en un momento.", LENGTH_SHORT).show();
                                 }
                             });
 
                     queue.add(jsonGetCustomerRequest);
-
-
-                    btnRegisterUser.setVisibility(View.VISIBLE);
-                    btnLookForContractNo.setVisibility(View.INVISIBLE);
-                    edtUserContract.setEnabled(false);
-                    edtUserEmail.setEnabled(true);
-                    edtUserPass.setEnabled(true);
-                    /*Boolean exists = db.customerExistsByContractNumber(contractNumber);
-                    db.close();
-                    if(exists){
-                        edtUserContract.setEnabled(false);
-                        edtUserEmail.setEnabled(true);
-                        edtUserPass.setEnabled(true);
-                        btnLookForContractNo.setVisibility(View.INVISIBLE);
-                        btnRegisterUser.setVisibility(View.VISIBLE);
-                    }*/
                 }
             }
         });
@@ -132,47 +124,49 @@ public class RegisterActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 if (edtUserEmail.getText() == null) {
-                    Toast.makeText(getApplicationContext(), "Por favor, escribe un correo.", Toast.LENGTH_SHORT).show();
+                    makeText(getApplicationContext(), "Por favor, escribe un correo.", LENGTH_SHORT).show();
                 } else if (edtUserPass.getText() == null) {
-                    Toast.makeText(getApplicationContext(), "Por favor, escribe una contraseña.", Toast.LENGTH_SHORT).show();
+                    makeText(getApplicationContext(), "Por favor, escribe una contraseña.", LENGTH_SHORT).show();
                 } else {
                     final String contractNumber = edtUserContract.getText().toString();
                     db.open();
-                    Boolean exists = db.customerExistsByContractNumber(contractNumber);
                     final Customer customer = db.getCustomerByContractNumber(contractNumber);
                     db.close();
-                    if(exists) {
-                        final String email = edtUserEmail.getText().toString();
-                        final String password = edtUserPass.getText().toString();
 
-                        final JSONObject customerJSONObject = new JSONObject();
-                        try {
-                            customerJSONObject.put("Email", email);
-                            customerJSONObject.put("Password", password);
-                            customerJSONObject.put("Registered", true);
-                            customerJSONObject.put("id", customer.getId());
-                        } catch (Exception e) {
-                            Toast.makeText(getApplicationContext(), "Oops, algo salio mal. Intenta en un momento.", Toast.LENGTH_SHORT);
-                        }
+                    final String email = edtUserEmail.getText().toString();
+                    final String password = edtUserPass.getText().toString();
 
-                        final JsonObjectRequest jsonUpdateCustomerRequest = new JsonObjectRequest
-                                (Request.Method.POST, customerAPI, customerJSONObject, new Response.Listener<JSONObject>() {
-                                    @Override
-                                    public void onResponse(JSONObject response) {
-                                        Toast.makeText(getApplicationContext(), "Bienvenido, " + customer.getName() + "!", Toast.LENGTH_SHORT).show();
-                                    }
-                                }, new Response.ErrorListener() {
-                                    @Override
-                                    public void onErrorResponse(VolleyError error) {
-                                        Toast.makeText(getApplicationContext(), "El registro no pudo ser completado, intente en un momento.", Toast.LENGTH_SHORT).show();
-                                    }
-                                });
-
-                        queue.add(jsonUpdateCustomerRequest);
-                    } else {
-                        Toast.makeText(getApplicationContext(), "El registro todavia no esta completo, intenta en unos segundos.", Toast.LENGTH_SHORT).show();
+                    final JSONObject customerJSONObject = new JSONObject();
+                    try {
+                        customerJSONObject.put("Email", email);
+                        customerJSONObject.put("Password", password);
+                        customerJSONObject.put("Registered", true);
+                        customerJSONObject.put("id", customer.getId());
+                    } catch (Exception e) {
+                        makeText(getApplicationContext(), "Oops, algo salio mal. Intenta en un momento.", LENGTH_SHORT);
                     }
 
+                    final JsonObjectRequest jsonUpdateCustomerRequest = new JsonObjectRequest
+                            (Request.Method.PUT, customerAPI, customerJSONObject, new Response.Listener<JSONObject>() {
+                                @Override
+                                public void onResponse(JSONObject response) {
+
+                                    try {
+                                        makeText(getApplicationContext(), "Bienvenido, " + response.getString("Name") + "!", LENGTH_SHORT).show();
+                                        Intent intent = new Intent(getApplicationContext(), UserLoginActivity.class);
+                                        startActivity(intent);
+                                    } catch (JSONException e) {
+                                        makeText(getApplicationContext(), "Algo salio mal, intenta de nuevo. :(", LENGTH_SHORT);
+                                    }
+                                }
+                            }, new Response.ErrorListener() {
+                                @Override
+                                public void onErrorResponse(VolleyError error) {
+                                    makeText(getApplicationContext(), "El registro no pudo ser completado, intente en un momento.", LENGTH_SHORT).show();
+                                }
+                            });
+
+                    queue.add(jsonUpdateCustomerRequest);
                 }
             }
         });
--- Burgerist/app/src/main/java/com/example/bretz/burgerist/Utils/DBUtils.java
+++ Burgerist/app/src/main/java/com/example/bretz/burgerist/Utils/DBUtils.java
@@ -11,7 +11,7 @@ import android.database.sqlite.SQLiteOpenHelper;
 public class DBUtils extends SQLiteOpenHelper {
 
     public static final String DATABASE_NAME = "Burgerista.db";
-    public static final int DATABASE_VERSION = 11;
+    public static final int DATABASE_VERSION = 13;
 
     //-----------------CUSTOMER TABLE---------------------------
     public static final String CUSTOMER_TABLE = "Customer";
